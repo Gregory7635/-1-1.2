8e3a2e874ee03a6dc71d4ef023a14f53c6512430
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-02-24 00:27:04 +0300

    Добавьте файлы проекта.

---
+++ лаб 1 задание 1.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+struct Book {
+    string title;
+    double price;
+};
+
+int main() {
+    stack<Book> bookStack;
+
+    Book b1 = { "book one", 431.87 };
+    Book b2 = { "book two", 410.99 };
+    Book b3 = { "book three", 345.50 };
+
+    bookStack.push(b1);
+    bookStack.push(b2);
+    bookStack.push(b3);
+
+    double totalSum = 0.0;
+    int bookCount = 0;
+
+    while (!bookStack.empty()) {
+        Book currentBook = bookStack.top();
+        totalSum += currentBook.price;
+        bookCount++;
+        std::cout << currentBook.title << ", " << currentBook.price << std::endl;
+        bookStack.pop();
+    }
+
+    double averagePrice = totalSum / bookCount;
+
+    cout << "Average book price: " << averagePrice << std::endl;
+
+    return 0;
+}
